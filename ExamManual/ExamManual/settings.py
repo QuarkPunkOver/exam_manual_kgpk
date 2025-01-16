e6755beff811775600fead9553635aa8099c7b24
funDAVEover <111georgiy111@gmail.com>
2025-01-16 21:43:38 +0300

    connect db postgres

--- ExamManual/ExamManual/settings.py
+++ ExamManual/ExamManual/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'ExamManual.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': '111',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
